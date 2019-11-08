4f1befe44c54c8351c518bd15482703370316299
gogo <zhang863576@126.com>
2019-11-08 10:36:23 +0800

    删除mainjs手动改写的代码，删除promise文件

--- nodeServer/src/template_str/vueTemp.js
+++ nodeServer/src/template_str/vueTemp.js
@@ -45,22 +45,8 @@ const methodStr = function (mName) {
         },
         `
 }
-
-
-const mainjsTemp = function () {
-    return `import Vue from 'vue';
-    import ElementUI from 'element-ui';
-    import 'element-ui/lib/theme-chalk/index.css';
-    import App from './App.vue';
-    Vue.use(ElementUI);
-    new Vue({
-        el: '#app',
-        render: h => h(App)
-    });`
-}
 module.exports = {
     tempStr,
-    mainjsTemp,
     fetchGetStr,
     dataStr,
     methodStr,
--- nodeServer/src/utils/exec.js
+++ nodeServer/src/utils/exec.js
@@ -3,7 +3,6 @@ const fs = require("fs");
 const os = require("os");
 const path = require("path")
 
-const { mainjsTemp } = require('../template_str/vueTemp');
 const { defaultProjectName } = require('../../constant');
 const presetFileName = 'preset.json'
 //基于此项目的preset的路径
@@ -12,7 +11,7 @@ const presetFilePath = '../config/' + presetFileName
 
 function exeCommand (file, data) {
     //生成preset文件，其它情况不用考虑； vue create --preset必要的
-    presetFileCreated(presetFilePath);
+    presetFileCreate(presetFilePath);
 
     //在用户目录下生成vue项目
     const result = exec("vue create --preset " + presetFileName + " " + defaultProjectName, { cwd : os.homedir() })
@@ -28,31 +27,25 @@ function exeCommand (file, data) {
     // 注册子进程关闭事件
     result.on('exit', function (code, signal) {
         console.log('child process eixt ,exit:' + code);
-        //1,重写vue项目中main.js
-        // let mainjsPath = os.homedir() + path.sep + defaultProjectName + path.sep + "src" + path.sep + "main.js"
-        // fs.writeFile(mainjsPath, mainjsTemp(), (error) => {
-        //     if (error) throw error;
-        //     console.log("main.js has been update")
-        // })
-        //2,创建[file].vue
+        //1,创建[file].vue
         fs.writeFile(file, data, (error) => {
             if (error) throw error;
             console.log(file + ".vue has been created")
         })
-        //3,删除preset
-        presetFileDeleted()
+        //2,删除preset
+        presetFileDelete()
     })
 }
-//vue cli preset 文件生成
-function presetFileCreated (filePath) {
+//vue cli preset 文件生成在用户目录
+function presetFileCreate (filePath) {
     if (!fs.existsSync(os.homedir() + path.sep + presetFileName))
         fs.writeFileSync(os.homedir() + path.sep + presetFileName, fs.readFileSync(filePath, 'utf-8'))
     else
         return false
 }
 
-//vue cli preset 文件删除
-function presetFileDeleted () {
+//vue cli preset 将用户目录的preset文件删除
+function presetFileDelete () {
     fs.unlink(os.homedir() + path.sep + presetFileName, (err) => {
         if (err) console.error(err)
     })
